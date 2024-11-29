3e587ceb8c2c823dc7f8ad68527544bbef4768a5
jhona166 <jdaltamiranoo@uce.edu.ec>
2024-11-28 19:40:29 -0500

    Ejercicio Practico

---
+++ js/formulario.js
@@ -0,0 +1,18 @@
+var cedula ='';
+var nombre='';
+var apellido='';
+
+function guardar(elemento){
+    texto = document.getElementById(elemento);
+    nombre=texto.innertext;
+    console.log(texto)
+
+if(elemento.length > 0){
+    console.log(elemento);
+}
+}
+function  validar(){
+
+
+
+}
